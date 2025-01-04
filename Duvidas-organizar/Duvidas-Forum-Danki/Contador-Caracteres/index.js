f87748aa172823f6c95c9403e53268e3c9117f08
Gustavo Teixeira <gustavo.teixeira.py@gmail.com>
2025-01-04 17:56:40 -0300

    Contador de caracteres Danki

---
+++ Duvidas-organizar/Duvidas-Forum-Danki/Contador-Caracteres/index.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('load', () => {
+	const textarea = document.getElementById('textarea');
+	const restante = document.getElementById('restante');
+	const maximo = 100;
+
+	textarea.addEventListener('input', function () {
+		const restanteCaractere = maximo - textarea.value.length;
+		restante.textContent = restanteCaractere + ' caracteres restantes.';
+	});
+});
