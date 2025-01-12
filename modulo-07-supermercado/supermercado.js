5afe1690cfe1b8c843e2d6d45f094b87e9644025
Gustavo Teixeira <gustavo.teixeira.py@gmail.com>
2025-01-11 23:34:05 -0300

    aula 03 lista supermercado adicionando e somando

--- modulo-07-supermercado/supermercado.js
+++ modulo-07-supermercado/supermercado.js
@@ -1,25 +1,51 @@
 // alert("Supermercado");
-var itens = [];
+var items = [];
 
 document.querySelector("input[type=button]").addEventListener("click", () => {
   // alert('Clicou')
 
-  let nome = document.querySelector(`input[name=nome-produto]`).value;
+  let nome = document.querySelector(`input[name=nome-produto]`);
   console.log(nome);
 
-  let valor = document.querySelector(`input[name=valor-produto]`).value;
+  let valor = document.querySelector(`input[name=valor-produto]`);
   console.log(valor);
 
-//   itens.push(nome, valor) - Uma forma de adicionar os valores ao array
-   itens.push({nome, valor})
+  //   itens.push(nome, valor) - Uma forma de adicionar os valores ao array
+  //   items.push({ nome, valor });
 
-//   Outra forma de adicionar os valores ao array
-//   itens.push({
-//     id: itens.length + 1,
-//     nome: nome,
-//     valor: valor,
+  // Outra forma de adicionar os valores ao array
+  items.push({
+    // id: items.length + 1,
+    name: nome.value,
+    price: valor.value,
+  });
+  console.log(items);
+
+  let listaProdutos = document.querySelector(".produtos-carrinho");
+  let soma = 0;
+  
+  console.log(listaProdutos);
+  
+  listaProdutos.innerHTML = "";
+  
+  items.map(function (val) {
+    listaProdutos.innerHTML += `<div class="produto-single">
+    <h3>${val.name}</h3>
+    <h3 class="price-produto"><span>R$ ${val.price}</span></h3>
+  </div>
+  `;
+  });
+
+  soma+= parseFloat(valor.value);
+  nome.value = "";
+  valor.value = "";
+
+  let total = document.querySelector(".total");
+  total = document.querySelector(".total").innerHTML = `Total: R$ ${soma}`;
+
+//   soma = items.reduce((acc, item) => {
+//     return acc + parseFloat(item.price);
 //   });
-
+  console.log(soma);
   // var item = document.querySelector('input[type=button]').value
-  console.log(itens);
 });
